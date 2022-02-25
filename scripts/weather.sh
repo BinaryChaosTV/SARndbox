b3bd9a211f389e50dabf7f23a71f68ca31e21f21
Sandbox <52639192+BinaryChaosTV@users.noreply.github.com>
2022-02-25 08:45:57 -0500

    Merged files

--- scripts/weather.sh
+++ scripts/weather.sh
@@ -10,12 +10,6 @@
         ;;
      "lava") sh ~/src/SARndbox-2.8/scripts/switch-to-snow.sh
         ;;
-<<<<<<< HEAD
      "snow") sh ~/src/SARndbox-2.8/scripts/switch-to-water.sh
-=======
-     "snow") sh ~/src/scripts/switch-to-erosion.sh
-        ;;
-     "erosion") sh ~/src/scripts/switch-to-water.sh
->>>>>>> b6dd75f3afae5e475a9ce0e861446e02fb073bf4
         ;;
     esac
